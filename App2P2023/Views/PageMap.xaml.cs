ed48032c9fbdd35bb51ca627482381116df312b6
CAAndinoS <124225358+CAAndinoS@users.noreply.github.com>
2023-06-13 19:54:53 -0600

    update 13/6/23

--- App2P2023/Views/PageMap.xaml.cs
+++ App2P2023/Views/PageMap.xaml.cs
@@ -7,6 +7,8 @@ using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
+using Plugin.Geolocator.Abstractions;
+using Plugin.Geolocator;
 
 namespace App2P2023.Views
 {
@@ -21,6 +23,33 @@ namespace App2P2023.Views
         {
             base.OnAppearing();
 
+            var conectividad = Connectivity.NetworkAccess;
+            var locl = CrossGeolocator.Current;
+
+            if (conectividad == NetworkAccess.Internet)
+            {
+                if (locl != null)
+                {
+                    locl.PositionChanged += Locl_PositionChanged;
+
+                    if (!locl.IsListening)
+                    {
+                        await locl.StartListeningAsync(TimeSpan.FromSeconds(10), 100);
+                    }
+
+                    var posicion = await locl.GetPositionAsync();
+                    var mapcenter = new Xamarin.Forms.Maps.Position(posicion.Latitude, posicion.Longitude);
+                    mapa.MoveToRegion(new MapSpan(mapcenter, 1, 1));
+                }
+            }
+            else 
+            {
+                var posicion = await locl.GetPositionAsync();
+                var mapcenter = new Xamarin.Forms.Maps.Position(posicion.Latitude, posicion.Longitude);
+                mapa.MoveToRegion(new MapSpan(mapcenter, 1, 1));
+            }
+
+            /*
             var location = await Geolocation.GetLocationAsync();
             if (location != null )
             {
@@ -29,9 +58,15 @@ namespace App2P2023.Views
                     Position = new Position(location.Latitude, location.Longitude), Label = "Ubicacion actual"
                 };
                 mapa.Pins.Add(pin);
-                mapa.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(location.Latitude, location.Longitude), Distance.FromMeters(100)));
-                
+                mapa.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(location.Latitude, location.Longitude), Distance.FromMeters(100)));             
             }
+            */
+        }
+
+        private void Locl_PositionChanged(object sender, PositionEventArgs e)
+        {
+            var mapcenter = new Xamarin.Forms.Maps.Position(e.Position.Latitude, e.Position.Longitude);
+            mapa.MoveToRegion(new MapSpan(mapcenter, 1, 1));
         }
     }
 }
